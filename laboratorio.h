6afc7f589f1f70cd571a425970ba9a17aa7175e4
Kevin Limón <kevin.limon7482@alumnos.udg.mx>
2020-10-12 16:33:18 -0500

    Implemente agregar computadora y mostrar en la clase laboratorio

---
+++ laboratorio.h
@@ -0,0 +1,16 @@
+#ifndef LABORATIORIO_H
+#define LABORATIORIO_H
+#include "computadora.h"
+
+class Laboratorio
+{
+    Computadora arreglo[5];
+    size_t cont;
+public:
+    Laboratorio();
+    void agregarComputadora(const Computadora &c);
+    void mostrar();
+};
+
+
+#endif
