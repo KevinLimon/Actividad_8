d62afb140989574da6b3f23f6b57e9ad233684b6
Kevin Limón <kevin.limon7482@alumnos.udg.mx>
2020-10-12 16:09:27 -0500

    Implemete los metodos de acceso para imprimir en pantalla un objeto de la clase Computadora

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 
 int main(){
-    Computadora c01 = Computadora("lenovo", "azul", 55.0, 4);
+    Computadora c01 = Computadora("lenovo", "azul", 55.0, 4); //La memoria es en GB
+
+    Computadora c02;
+    c02.setMarca("HP");
+    c02.setColor("negro");
+    c02.setPrecio(1200.0);
+    c02.setMemoria(1024); //La memoria es en GB
+
+    cout<<c02.getMarca()<<endl;
+    cout<<c02.getColor()<<endl;
+    cout<<c02.getPrecio()<<endl;
+    cout<<c02.getMemoria()<<endl;
     return 0;
 }
